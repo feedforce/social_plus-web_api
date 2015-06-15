270f5b51c9b09faadb1b06ab41565445157dce07
OZAWA Sakuro <sakuro@users.noreply.github.com>
2015-06-15 10:31:27 +0900

    Accept only :expect syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,13 @@
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    expectations.syntax = %I(expect)
   end
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
+    mocks.syntax = %I(expect)
   end
 end
