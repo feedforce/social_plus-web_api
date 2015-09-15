5613f0a633eb2291a95b894a108022f9fb796915
kasei_san <kasei.san@gmail.com>
2015-09-15 12:20:30 +0900

    Add InvalidToken(Exception)

---
+++ lib/social_plus/web_api/invalid_token.rb
@@ -0,0 +1,10 @@
+require 'net/http'
+
+# :nodoc:
+module SocialPlus
+  # :nodoc:
+  module WebApi
+    # An Exception class which invalid token from SocialPlus Web API
+    class InvalidToken < StandardError; end
+  end
+end
